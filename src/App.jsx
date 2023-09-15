7f6cc6d31921959d89f7dd05cf6b0568bfb7f414
Mahfujur-Rahman6793 <mahfujurrahman6793@gmail.com>
2023-09-15 11:48:35 +0600

    added propstypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Blogs from './component/Blogs/Blogs'
 import Header from './component/header/Header'
+import BookMarks from './component/bookmarks/BookMarks';
 function App() {
   
 
   return (
     <>
       <Header></Header>
+      <div className='md:flex'>
       <Blogs></Blogs>
+      <BookMarks></BookMarks>
+      </div>
       
     </>
   )
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from '../../assets/image/02.jpg'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-3xl font-bold underline text-red-300'>Knowledge cafe</h1>
+        <div className='flex justify-between items-center p-2 mx-4 border-b-2'>
+            <h1 className='text-3xl font-bold  text-red-300'>Knowledge cafe</h1>
+            <img className='profile-image rounded-full' src={profile} alt="" />
         </div>
     );
 };
